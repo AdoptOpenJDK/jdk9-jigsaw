076b329156bc1d4fd582185ec95d949fc9075af3
neomatrix369 <sadhak001@gmail.com>
2017-04-19 18:29:08 +0100

    Added comments to explain whats happening in these scripts (what each command is doing and what the parameters mean).
    Also updated echos a bit to make the messages to make learning easier.

--- session-1-jigsaw-intro/03_MultiModuleCompilation/multiModCompile.sh
+++ session-1-jigsaw-intro/03_MultiModuleCompilation/multiModCompile.sh
@@ -2,12 +2,32 @@
 
 set -eu
 
+# Escape code
+esc=$(echo -en "\033")
+
+info="${esc}[0;33m"
+normal=$(echo -en "${esc}[m\017")
+
 DESTINATION_FOLDER="mods"
-echo "Creating folder $DESTINATION_FOLDER, if it does not exists."
-mkdir -p $DESTINATION_FOLDER
 
-echo "Compiling modules in $DESTINATION_FOLDER"
+echo ""
+echo "${info} *** Displaying the contents (source files) of the 'src' folder *** ${normal}"
+tree -fl src
+
+echo ""
+echo "${info} *** Compiling both modules into the '$DESTINATION_FOLDER' folder *** ${normal}"
 javac -d $DESTINATION_FOLDER \
-  --module-source-path src $(find . -name "*.java")
+      --module-source-path src $(find . -name "*.java")
 
+# *************************************************************************************
+#
+# The --module-source-path parameter of javac takes input source files for multiple modules.
+# In the above both the modules 'org.astro' and 'com.greetings' are compiled at the same time, dependency is fulfilled immediately.
+#
+# *************************************************************************************
+
+echo ""
+echo "${info} *** Displaying the contents (modules) of the 'mods' folder *** ${normal}"
 tree -fl $DESTINATION_FOLDER
+
+# See ../01_Greetings/compile.sh for explanations to above commands
