1223d4f8bf82b5a6429fbf9b623d63d2027d3ed7
neomatrix369 <sadhak001@gmail.com>
2017-04-19 19:16:19 +0100

    Added comments to explain whats happening in these scripts (what each command is doing and what the parameters mean).
    Also updated echos a bit to make the messages to make learning easier.

--- session-1-jigsaw-intro/04_Packaging/packing.sh
+++ session-1-jigsaw-intro/04_Packaging/packing.sh
@@ -2,23 +2,56 @@
 
 set -eu
 
-mkdir -p mlib
+# Escape code
+esc=$(echo -en "\033")
 
-echo "Creating module org.astro as a jar"
-jar --create --file mlib/org.astro@1.0.jar \
-	--module-version 1.0 -C mods/org.astro .
+info="${esc}[0;33m"
+normal=$(echo -en "${esc}[m\017")
 
-echo
-echo "Creating module com.greetings as a jar"
-jar --create --file mlib/com.greetings.jar \
-	--main-class=com.greetings.Main -C mods/com.greetings .
+echo ""
+echo "${info} *** Creating folder 'mlib' for the packages (jar files) to be created *** ${normal}"
+mkdir -p mlib              # this must be created in order for the jar commands below to be successful
 
+echo ""
+echo "${info} *** Creating module org.astro as a package file (jar) *** ${normal}"
+jar --create \
+    --file mlib/org.astro@1.0.jar \
+	--module-version 1.0 \
+	-C mods/org.astro .
+
+echo ""
+echo "${info} *** Creating module com.greetings as a package file (jar) *** ${normal}"
+jar --create \
+    --file mlib/com.greetings.jar \
+	--main-class=com.greetings.Main \
+	-C mods/com.greetings .
+
+echo ""
+echo "${info} *** Displaying the contents (package files created) of the 'mlib' folder *** ${normal}"
 tree mlib
 
-echo
-echo "Printing module description for org.astro"
-jar --print-module-descriptor --file=mlib/org.astro@1.0.jar
+echo ""
+echo "${info} *** Printing module description for org.astro as recorded in the module-info.class file in the package (jar) *** ${normal}"
+jar --verbose \
+    --describe-module \
+    --file=mlib/org.astro@1.0.jar
 
-echo
-echo "Printing module description for com.greetings"
-jar --print-module-descriptor --file=mlib/com.greetings.jar
+    echo ""
+echo "${info} *** Displaying contents of the module package org.astro@1.0 *** ${normal}"
+jar --verbose \
+    --list \
+    --file=mlib/org.astro@1.0.jar
+
+echo ""
+echo "${info} *** Printing module description for com.greetings as recorded in the module-info.class file in the package (jar) *** ${normal}"
+jar --verbose \
+    --describe-module \
+    --file=mlib/com.greetings.jar
+
+    echo ""
+echo "${info} *** Displaying contents of the module package com.greetings *** ${normal}"
+jar --verbose \
+    --list \
+    --file=mlib/com.greetings.jar
+
+# Run 'jar --help' and 'jar --help-extra' to learn about all the above parameters used to create package files (jar) and also read their descriptions
--- session-1-jigsaw-intro/04_Packaging/run.sh
+++ session-1-jigsaw-intro/04_Packaging/run.sh
@@ -2,8 +2,17 @@
 
 set -eu
 
+# Escape code
+esc=$(echo -en "\033")
+
+info="${esc}[0;33m"
+normal=$(echo -en "${esc}[m\017")
+
+echo ""
+echo "${info} *** Running 'com.greetings.Main' from within the module package in the 'mlib' folder (running 'org.astro.World' from within the module package in the 'mlib' folder) *** ${normal}"
 echo 
-echo "Running 'com.greetings.Main' from within the module (jar) in the mlib folder."
-echo "Dependent on 'org.astro.World' from within the module (jar) in the mlib folder."
-echo 
-java --module-path mlib --module com.greetings
+java --module-path mlib \
+     --module com.greetings
+
+# See ../01_Greetings/run.sh for explanations to above commands
+# Also see how the --module-path can read module packages (jar files) in 'mlib' and take them as source modules for execution
