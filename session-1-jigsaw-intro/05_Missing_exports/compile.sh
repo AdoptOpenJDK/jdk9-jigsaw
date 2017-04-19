b45782965732dc2e346f9b295a606958e6e86321
neomatrix369 <sadhak001@gmail.com>
2017-04-19 19:52:40 +0100

    Added comments to explain whats happening in the compile script (what each command is doing and what the parameters mean).
    Also updated echos a bit to make the messages to make learning easier.

--- session-1-jigsaw-intro/05_Missing_exports/compile.sh
+++ session-1-jigsaw-intro/05_Missing_exports/compile.sh
@@ -2,18 +2,39 @@
 
 set -eu
 
+# Escape code
+esc=$(echo -en "\033")
+
+info="${esc}[0;33m"
+normal=$(echo -en "${esc}[m\017")
+
 COM_GREETINGS_FOLDER="mods/com.greetings"
 ORG_ASTRO_FOLDER="mods/org.astro"
 
-echo "Creating folders $ORG_ASTRO_FOLDER and $COM_GREETINGS_FOLDER, if they do not exists."
-mkdir -p $ORG_ASTRO_FOLDER $COM_GREETINGS_FOLDER
-
-echo "Compiling modules in $ORG_ASTRO_FOLDER"
+echo ""
+echo "${info} *** Compiling modules in $ORG_ASTRO_FOLDER (with requires commented out) *** ${normal}"
 javac -d $ORG_ASTRO_FOLDER \
-        src/org.astro/module-info.java src/org.astro/org/astro/World.java
+       src/org.astro/module-info.java \
+       src/org.astro/org/astro/World.java
+
+echo ""
+echo "${info} *** Displaying the contents (modules) of the '$ORG_ASTRO_FOLDER' folder *** ${normal}"
 tree -fl $ORG_ASTRO_FOLDER
 
-echo "Compiling modules in $COM_GREETINGS_FOLDER"
-javac --module-path mods -d $COM_GREETINGS_FOLDER \
-        src/com.greetings/module-info.java src/com.greetings/com/greetings/Main.java
+echo ""
+echo "${info} *** Compiling modules in $COM_GREETINGS_FOLDER (fails with an error due to module 'org.astro' not being visible) *** ${normal}"
+javac --module-path mods \
+      -d $COM_GREETINGS_FOLDER \
+      src/com.greetings/module-info.java \
+      src/com.greetings/com/greetings/Main.java
 
+# ********************************************************************************
+# Compilation does not finish due to compilation error, no artifacts are created.
+# Execution does not proceed further, script exists at this point.
+# ********************************************************************************
+
+echo ""
+echo "${info} *** Displaying the contents (modules) of the '$COM_GREETINGS_FOLDER' folder *** ${normal}"
+tree -fl $$COM_GREETINGS_FOLDER
+
+# See ../01_Greetings/compile.sh for explanations to above commands
