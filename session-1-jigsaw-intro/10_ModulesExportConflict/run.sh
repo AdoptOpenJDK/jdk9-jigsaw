e97090e4f7ef7ed7b413178d3f80f261a02e36a9
neomatrix369 <sadhak001@gmail.com>
2017-04-20 22:08:42 +0100

    Added comments to explain whats happening in the compile script (what each command is doing and what the parameters mean).
    Also updated echos a bit to make the messages to make learning easier.

--- session-1-jigsaw-intro/10_ModulesExportConflict/run.sh
+++ session-1-jigsaw-intro/10_ModulesExportConflict/run.sh
@@ -2,8 +2,15 @@
 
 set -eu
 
+# Escape code
+esc=$(echo -en "\033")
+
+info="${esc}[0;33m"
+normal=$(echo -en "${esc}[m\017")
+
 echo 
-echo "Running 'com.greetings.Main' from within the mods folder."
-echo "Will fail because 'org.astro' and 'org.astro2' packages are exporting the same module 'org.astro'."
+echo "${info} *** Running 'com.greetings.Main' from within the mods folder. *** ${normal}"
+echo "${info} *** Will fail because 'org.astro' and 'org.astro2' packages are exporting the same module 'org.astro'. *** ${normal}"
 echo 
-java --module-path mods --module com.greetings/com.greetings.Main
+java --module-path mods \
+     --module com.greetings/com.greetings.Main
