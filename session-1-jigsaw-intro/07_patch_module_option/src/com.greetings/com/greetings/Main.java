2a13c86ae047d34005783555b6e1ceb86d21746d
Alasdair Collinson <blalasaadri@googlemail.com>
2017-04-22 06:19:55 +0000

    Fixed patch example

---
+++ session-1-jigsaw-intro/07_patch_module_option/src/com.greetings/com/greetings/Main.java
@@ -0,0 +1,9 @@
+package com.greetings;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new ConcurrentHashMap());
+    }
+}
