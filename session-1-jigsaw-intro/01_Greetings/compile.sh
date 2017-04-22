5c688cd73baa0c69c0e748de3aa5af1d24e1bc1b
Marius Kruger <amanic@gmail.com>
2017-04-22 15:37:04 +0200

    factor common code out from first example as per review feedback

--- session-1-jigsaw-intro/01_Greetings/compile.sh
+++ session-1-jigsaw-intro/01_Greetings/compile.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-set -eu
-
-# Escape code
-esc=$(echo -en "\033")
-
-info="${esc}[0;33m"
-normal=$(echo -en "${esc}[m\017")
-
-java -version 2>&1 | grep -q '"9' || echo "**** Fix your PATH! ****" && java -version && exit 1
+source ../../common.sh
 
 COM_GREETINGS_FOLDER="mods/com.greetings"
 
