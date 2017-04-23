ff6e2de9a300af51f74fcd35bc10a00c2f38b675
Alasdair Collinson <blalasaadri@googlemail.com>
2017-04-23 11:31:06 +0000

    Fixed formatting

--- session-1-jigsaw-intro/08_patch_ConcurrentHashMap/src/java.base/java/util/concurrent/ConcurrentHashMap.java
+++ session-1-jigsaw-intro/08_patch_ConcurrentHashMap/src/java.base/java/util/concurrent/ConcurrentHashMap.java
@@ -3,71 +3,75 @@ package java.util.concurrent;
 import java.io.Serializable;
 import java.util.*;
 
-public class ConcurrentHashMap<K,V> extends AbstractMap<K,V> implements ConcurrentMap<K,V>, Serializable {
-	private final Map<K, V> innerMap;
+public class ConcurrentHashMap<K, V> extends AbstractMap<K, V> implements ConcurrentMap<K, V>, Serializable {
+    private final Map<K, V> innerMap;
 
-	public ConcurrentHashMap() {
-		this.innerMap = new HashMap<>();
-	}
+    public ConcurrentHashMap() {
+        this.innerMap = new HashMap<>();
+    }
 
-	public ConcurrentHashMap(int initialCapacity) {
-		this.innerMap = new HashMap<>(initialCapacity);
-	}
+    public ConcurrentHashMap(int initialCapacity) {
+        this.innerMap = new HashMap<>(initialCapacity);
+    }
 
-	public ConcurrentHashMap(int initialCapacity,
-							 float loadFactor, int concurrencyLevel) {
-		this.innerMap = new HashMap<>(initialCapacity, loadFactor);
-	}
+    public ConcurrentHashMap(int initialCapacity,
+                             float loadFactor, int concurrencyLevel) {
+        this.innerMap = new HashMap<>(initialCapacity, loadFactor);
+    }
 
-	public Set<Map.Entry<K,V>> entrySet() {
-		return innerMap.entrySet();
-	}
+    public Set<Map.Entry<K, V>> entrySet() {
+        return innerMap.entrySet();
+    }
 
-	@Override
-	public boolean replace(K key, V oldValue, V newValue) {
-		return innerMap.replace(key, oldValue, newValue);
-	}
+    @Override
+    public boolean replace(K key, V oldValue, V newValue) {
+        return innerMap.replace(key, oldValue, newValue);
+    }
 
-	@Override
-	public V replace(K key, V value) {
-		return innerMap.replace(key, value);
-	}
+    @Override
+    public V replace(K key, V value) {
+        return innerMap.replace(key, value);
+    }
 
-	@Override
-	public V remove(Object o) { return innerMap.remove(o); }
+    @Override
+    public V remove(Object o) {
+        return innerMap.remove(o);
+    }
 
-	@Override
-	public boolean remove(Object key, Object value) { return innerMap.remove(key, value); }
+    @Override
+    public boolean remove(Object key, Object value) {
+        return innerMap.remove(key, value);
+    }
 
-	@Override
-	public V putIfAbsent(K key, V value) {
-		return innerMap.putIfAbsent(key, value);
-	}
+    @Override
+    public V putIfAbsent(K key, V value) {
+        return innerMap.putIfAbsent(key, value);
+    }
 
-	@Override
-	public V put(K key, V value) {
-		return innerMap.put(key, value);
-	}
+    @Override
+    public V put(K key, V value) {
+        return innerMap.put(key, value);
+    }
 
-	@Override
-	public V get(Object key) {
-		return innerMap.get(key);
-	}
+    @Override
+    public V get(Object key) {
+        return innerMap.get(key);
+    }
 
-	@Override
-	public void putAll(Map<? extends K, ? extends V> map) {
-		innerMap.putAll(map);
-	}
+    @Override
+    public void putAll(Map<? extends K, ? extends V> map) {
+        innerMap.putAll(map);
+    }
 
     @Override
     public boolean containsKey(Object key) {
-	    if (Objects.equals(key,     "Duke")) {
-	        return true;
+        if (Objects.equals(key, "Duke")) {
+            return true;
         }
         return super.containsKey(key);
     }
 
-	public String toString() {
-		return "patched ConcurrentHashMap " + super.toString() + " + Duke";
-	}
+    public String toString() {
+        return "patched ConcurrentHashMap " + super.toString() + " + Duke";
+    }
 }
