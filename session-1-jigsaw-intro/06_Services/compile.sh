a5f3053b2f399b2d48dc610bf1fcdc7eca3a9a79
neomatrix369 <sadhak001@gmail.com>
2017-04-27 00:03:51 +0100

    Removed redundant blocks of code in compile.sh and run.sh, and replaced it with call to 'source ../../common-functions.sh' instead

--- session-1-jigsaw-intro/06_Services/compile.sh
+++ session-1-jigsaw-intro/06_Services/compile.sh
@@ -2,20 +2,7 @@
 
 set -eu
 
-# Escape code
-esc=$(echo -en "\033")
-
-info="${esc}[0;33m"
-normal=$(echo -en "${esc}[m\017")
-
-runTree() 
-{
-  if [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] ; then
-    cmd //c "tree /f /a $1"
-  else
-    tree -fl $1
-  fi
-}
+source ../../common-functions.sh
 
 DESTINATION_FOLDER="mods"
 
