2be314a72fb8e24b7f79add6651979badafa2265
neomatrix369 <sadhak001@gmail.com>
2017-04-27 00:07:27 +0100

    Removed redundant blocks of code in compile.sh and run.sh, and replaced it with call to 'source ../../common-functions.sh' instead

--- session-1-jigsaw-intro/07_patch_ConcurrentHashMap/run.sh
+++ session-1-jigsaw-intro/07_patch_ConcurrentHashMap/run.sh
@@ -2,11 +2,7 @@
 
 set -eu
 
-# Escape code
-esc=$(echo -en "\033")
-
-info="${esc}[0;33m"
-normal=$(echo -en "${esc}[m\017")
+source ../../common-functions.sh
 
 echo 
 echo "${info} *** Running without the patch from within the mods folder. *** ${normal}"
