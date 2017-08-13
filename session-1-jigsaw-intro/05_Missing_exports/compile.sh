004a83d6a805d9498c914ea38a0eb8a8940ea993
neomatrix369 <sadhak001@gmail.com>
2017-08-14 00:14:29 +0100

    Adding first exercise 5A for session 1 - added hints and comments to be replaced by expected commands in order to be able to compile code

---
+++ session-1-jigsaw-intro/05_Missing_exports/compile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eu
+
+source ../../common-functions.sh
+
+COM_GREETINGS_FOLDER="mods/com.greetings"
+ORG_ASTRO_FOLDER="mods/org.astro"
+
+echo ""
+echo "${info} *** Compiling modules in $ORG_ASTRO_FOLDER (with 'exports' commented out) *** ${normal}"
+javac -d $ORG_ASTRO_FOLDER \
+       src/org.astro/module-info.java \
+       src/org.astro/org/astro/World.java
+
+echo ""
+echo "${info} *** Displaying the contents (modules) of the '$ORG_ASTRO_FOLDER' folder *** ${normal}"
+runTree "$ORG_ASTRO_FOLDER"
+
+echo ""
+echo "${info} *** Compiling modules in $COM_GREETINGS_FOLDER (fails with an error due to module 'org.astro' not being visible) *** ${normal}"
+javac --module-path mods \
+      -d $COM_GREETINGS_FOLDER \
+      src/com.greetings/module-info.java \
+      src/com.greetings/com/greetings/Main.java
+
+# ********************************************************************************
+# Compilation does not finish due to compilation error, no artifacts are created.
+# Execution does not proceed further, script exists at this point.
+# ********************************************************************************
+
+echo ""
+echo "${info} *** Displaying the contents (modules) of the '$COM_GREETINGS_FOLDER' folder *** ${normal}"
+runTree "$COM_GREETINGS_FOLDER"
+
+# See ../01_Greetings/compile.sh for explanations to above commands
