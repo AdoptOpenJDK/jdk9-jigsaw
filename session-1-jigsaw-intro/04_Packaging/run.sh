299ac8dddcbad334c62c15576fc2ed161f5328d9
neomatrix369 <sadhak001@gmail.com>
2017-08-14 00:10:47 +0100

    Added another additional example to the run.sh in 04 Packaging exercise to show a contrast between the two ways of running modules

--- session-1-jigsaw-intro/04_Packaging/run.sh
+++ session-1-jigsaw-intro/04_Packaging/run.sh
@@ -10,5 +10,11 @@ echo
 java --module-path mlib \
      --module com.greetings
 
+echo ""
+echo "${info} *** Running 'com.greetings.Main' from within the module package in the 'mods' folder (running 'org.astro.World' from within the module package in the 'mods' folder) *** ${normal}"
+echo
+java --module-path mods \
+     --module com.greetings/com.greetings.Main
+
 # See ../01_Greetings/run.sh for explanations to above commands
 # Also see how the --module-path can read module packages (jar files) in 'mlib' and take them as source modules for execution
