427aa0aa45f1fe37ba567d55ceb08ced200fad27
neomatrix369 <sadhak001@gmail.com>
2017-08-13 23:45:08 +0100

    Adding first exercise 3 for session 1 - added hints and comments to be replaced by expected commands in order to be able to compile code

---
+++ session-1-jigsaw-intro/03_MultiModuleCompilation/multiModCompile.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu
+
+source ../../common-functions.sh
+
+DESTINATION_FOLDER="mods"
+
+echo ""
+echo "${info} *** Displaying the contents (source files) of the 'src' folder *** ${normal}"
+runTree src
+
+echo ""
+echo "${info} *** Compiling both modules into the '$DESTINATION_FOLDER' folder *** ${normal}"
+javac -d $DESTINATION_FOLDER \
+      [parameter to point to the folder with source files in a module] src $(find . -name "*.java")
+
+#
+# * look for documentation in the Java 9 Resource (https://github.com/AdoptOpenJDK/jdk9-jigsaw/blob/master/Java-9-Resources.md).
+#
+
+# *************************************************************************************
+#
+# ...
+# In the above both the modules 'org.astro' and 'com.greetings' are compiled at the same time, dependency is fulfilled immediately.
+#
+# *************************************************************************************
+
+echo ""
+echo "${info} *** Displaying the contents (modules) of the 'mods' folder *** ${normal}"
+runTree "$DESTINATION_FOLDER"
+
+# See ../01_Greetings/compile.sh for explanations to above commands
---
+++ session-1-jigsaw-intro/03_MultiModuleCompilation/src/com.greetings/com/greetings/Main.java
@@ -0,0 +1,9 @@
+package com.greetings;
+
+import org.astro.World;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.format("Greetings %s!%n", World.name());
+	}
+}
