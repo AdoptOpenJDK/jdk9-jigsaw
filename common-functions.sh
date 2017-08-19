b427818a94f50abba5299f2922dcb28b73e14596
neomatrix369 <sadhak001@gmail.com>
2017-08-19 17:09:17 +0100

    Fixed the script that fixes the javac version checking issue

--- common-functions.sh
+++ common-functions.sh
@@ -11,7 +11,7 @@ info="${esc}[0;33m"
 normal=$(echo -en "${esc}[m\017")
 
 java -version 2>&1 | grep -q '"9' || (echo "**** Fix your PATH! Cannot find command 'java' (version 9) ****" && java -version && exit 1)
-javac -version 2>&1 | grep -q '9-' || (echo "**** Fix your PATH! Cannot find command 'javac' (version 9) ****" && javac -version && exit 1)
+javac -version 2>&1 | grep -q '9' || (echo "**** Fix your PATH! Cannot find command 'javac' (version 9) ****" && javac -version && exit 1)
 
 runTree()
 {
