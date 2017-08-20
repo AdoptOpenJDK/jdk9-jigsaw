99a8731b9f82e1d51e8016f94e277f2275df55d3
Christian Stein <sormuras@gmail.com>
2017-08-21 00:22:54 +0200

    This PR introduces a Java Shell Builder named Bach.java
    It provides support for building projects within a JShell
    session or execute a build script, written in pure Java,
    in batch mode with:
    
        jshell build.jsh
    
    For details see https://github.com/sormuras/bach
    
    (cherry picked from commit 46cb9e1)

---
+++ session-3-jshell/bach-building-with-jshell/session-1-jigsaw-intro/Build_Session_1_JigsawIntro_03_MultiModuleCompilation.java
@@ -0,0 +1,94 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/** Multi module compilation using basic tools from {@code Bach.java} */
+class Build_Session_1_JigsawIntro_03_MultiModuleCompilation {
+
+  private static final String session = "session-1-jigsaw-intro";
+  private static final String example = "03_MultiModuleCompilation";
+  private static final Path source = Paths.get("..", "..", session, example, "src");
+  private static final Path modules = Paths.get("target", session, example, "mods");
+
+  /**
+   * Entry-point for the build.
+   *
+   * <p>The entry-point may be invoked by external programs, like IDEs and {@code jshell}.
+   *
+   * @param args build arguments - unused here
+   */
+  public static void main(String... args) {
+    /*
+     * Let bach print what is going on at the moment.
+     */
+    System.setProperty("bach.verbose", "true");
+
+    /*
+     * Create and configure `Javac` instance.
+     *
+     * The property names declared are the camel-case variants of the command line tool pendants.
+     *
+     * Line by line explanation:
+     *
+     * - JdkTool.Javac javac = new JdkTool.Javac();
+     *
+     *   Create Javac instance. It is initialized with (opinionated) default
+     *   option values. See command dump below.
+     *
+     *
+     * - javac.moduleSourcePath = List.of(source);
+     *
+     *   Define single Path instance (`source`) as the module path. This is
+     *   where `javac` finds input source files for multiple modules. It is
+     *   used by the terminal `run()` method, too.
+     *
+     *
+     * - javac.destinationPath = modules;
+     *
+     *   Set destination directory for compiled modules files.
+     *
+     *
+     * - javac.run();
+     *
+     *   Execute `javac` with configured options applied and all .java files
+     *   found by walking `javac.moduleSourcePath` recursively added.
+     *
+     *
+     * Command dump:
+     *  .
+     *  | javac
+     *  | -deprecation
+     *  | -d
+     *  |   target\session-1-jigsaw-intro\03_MultiModuleCompilation\mods
+     *  | -encoding
+     *  |   UTF-8
+     *  | -Werror
+     *  | --module-source-path
+     *  |   ..\..\session-1-jigsaw-intro\03_MultiModuleCompilation\src
+     *  | -parameters
+     *  | ..\..\session-1-jigsaw-intro\03_MultiModuleCompilation\src\com.greetings\com\greetings\Main.java
+     *  | ..\..\session-1-jigsaw-intro\03_MultiModuleCompilation\src\com.greetings\module-info.java
+     *  | ..\..\session-1-jigsaw-intro\03_MultiModuleCompilation\src\org.astro\module-info.java
+     *  | ..\..\session-1-jigsaw-intro\03_MultiModuleCompilation\src\org.astro\org\astro\World.java
+     *  '
+     */
+    JdkTool.Javac javac = new JdkTool.Javac();
+    javac.moduleSourcePath = List.of(source);
+    javac.destinationPath = modules;
+    javac.run();
+
+    /*
+     * Start the module.
+     *
+     * Command dump:
+     *  .
+     *  | java
+     *  | -p
+     *  |   target\session-1-jigsaw-intro\03_MultiModuleCompilation\mods
+     *  | -m
+     *  |   com.greetings/com.greetings.Main
+     *  '
+     */
+    JdkTool.run("java", "-p", modules, "-m", "com.greetings/com.greetings.Main");
+  }
+}
