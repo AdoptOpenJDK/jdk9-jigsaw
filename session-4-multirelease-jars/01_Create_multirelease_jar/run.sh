947114af32f1640c9ce8749efc41adfec53d93c0
Martin <marto8808@gmail.com>
2018-02-19 01:20:36 +0200

    Added exercise (session 4) on multirelease JAR files.

---
+++ session-4-multirelease-jars/01_Create_multirelease_jar/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -eu
+
+java -cp multirelease.jar com.project.Main
---
+++ session-4-multirelease-jars/01_Create_multirelease_jar/src/Project_JDK9/com/project/ProcessManager.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.Process;
+import java.lang.Runtime;
+import java.util.List;
+import java.util.LinkedList;
+
+public class ProcessManager {
+	
+	public List<String> allProcesses() {
+		// TODO - your code here
+	}
+}
